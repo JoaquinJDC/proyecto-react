6baed282054478de1eb31fd8712d43da403b7e69
Joaquin Colombo <99906461+JoaquinJDC@users.noreply.github.com>
2022-03-09 11:45:49 -0300

    Entrega NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
+    
     <div className="App">
+      <NavBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
